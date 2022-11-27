17152cbfac5134e7c5661b22b52399de81e7f6bd
bochil132 <117184347+bochil132@users.noreply.github.com>
2022-11-28 00:20:44 +0700

    Update and rename cekxray.sh to cek.sh

--- xray/cek.sh
+++ xray/cek.sh
@@ -24,10 +24,12 @@ do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
+
 echo -n > /tmp/ipvmess.txt
 data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
+
 jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/ipvmess.txt
@@ -37,16 +39,20 @@ fi
 jum2=$(cat /tmp/ipvmess.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
+
 jum=$(cat /tmp/ipvmess.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
 jum2=$(cat /tmp/ipvmess.txt | nl)
-echo "user : $akun";
-echo "$jum2";
-echo "----------------------------------------"
+echo -e "user : $akun";
+echo -e "$jum2";
+fi
 rm -rf /tmp/ipvmess.txt
+done
+
 rm -rf /tmp/other.txt
+echo "----------------------------------------"
 echo -e ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
