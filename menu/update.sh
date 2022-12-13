2f0e3d6c6cae3165c03df64de8ae86d56ae0890c
bochil132 <117184347+bochil132@users.noreply.github.com>
2022-12-13 16:04:19 +0700

    Create update.sh

---
+++ menu/update.sh
@@ -0,0 +1,18 @@
+clear
+echo -e ""
+wget -q -O /usr/bin/menu "https://autosc.mybochil.me/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/style-ssh "https://autosc.mybochil.me/menu/style-ssh.sh" && chmod +x /usr/bin/style-ssh
+wget -q -O /usr/bin/style-vmess "https://autosc.mybochil.me/menu/style-vmess.sh" && chmod +x /usr/bin/style-vmess
+wget -q -O /usr/bin/style-bw "https://autosc.mybochil.me/menu/style-bw.sh" && chmod +x /usr/bin/style-bw
+sleep 1
+echo -e "-------------------------------" | lolcat
+echo -e "Done Update All Menu Script.!!"
+echo -e "-------------------------------" | lolcat
+echo -e "Type [ menu ] to continue"
+sleep 2
+rm -rf update.sh
+
+
+
+
+
