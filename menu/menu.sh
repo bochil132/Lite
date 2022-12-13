cd06cf64ed0129b9d6fe97cbc1ee756098cfde4c
bochil132 <117184347+bochil132@users.noreply.github.com>
2022-12-13 20:16:10 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -484,8 +484,9 @@ menu
 
 echo -e ""
 clear
-data=$(curl -sS https://raw.githubusercontent.com/bochil132/permission/main/ipmini | grep $MYIP | awk '{print $3}')
-name=$(curl -sS https://raw.githubusercontent.com/bochil132/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+ipnya=$(curl -sS ipv4.icanhazip.com)
+data=$(curl -sS https://raw.githubusercontent.com/bochil132/permission/main/ipmini | grep $ipnya | awk '{print $3}')
+name=$(curl -sS https://raw.githubusercontent.com/bochil132/permission/main/ipmini | grep $ipnya | awk '{print $2}')
 up="$(uptime -p | cut -d " " -f 2-10)"
 cpu=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
 core=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
